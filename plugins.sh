7e863a9804c0b115547495608cd27ed5ceb607b5
Wang Xiaoqiang <wxq521@gmail.com>
2014-06-11 16:25:25 +0800

    Update plugins.sh
    install vim-json.vim and good looking of log()

--- plugins.sh
+++ plugins.sh
@@ -14,30 +14,62 @@ COLO_LGR='\e[0;37m' ; COLO_WHT='\e[1;37m'
 COLO_NC='\e[0m'
 
 log() {
-  printf "${COLO_GRN}[%24s] --- :${COLO_NC} $1\n" "$(date +"%Y-%m-%d %H:%M:%S.%6N")"
+  case "$2" in
+    "good")
+      printf "${COLO_YLW}[%24s] :${COLO_NC} ${COLO_GRN}$1${COLO_NC}\n" "$(date +"%Y-%m-%d %H:%M:%S.%6N")"
+      ;;
+    "bad")
+      printf "${COLO_YLW}[%24s] :${COLO_NC} ${COLO_RED}$1${COLO_NC}\n" "$(date +"%Y-%m-%d %H:%M:%S.%6N")"
+      ;;
+    *)
+      printf "${COLO_YLW}[%24s] :${COLO_NC} $1\n" "$(date +"%Y-%m-%d %H:%M:%S.%6N")"
+      ;;
+  esac
 }
 
+requirements="git curl"
+log
+for cmd in $requirements; do
+  which $cmd >/dev/null 2>&1 || {
+    log
+    log "command [$cmd] is not installed at all." bad
+    log
+    exit 1
+  }
+  log "check command [$cmd] is good." good
+done
+log
 
-log "install pathogen.vim" 
+log
+log "install pathogen.vim"
 test -f ~/.vim/autoload/pathogen.vim || {
   mkdir -p ~/.vim/autoload ~/.vim/bundle && \
     curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
   test $? -eq 0 || {
-    log "fail."
+    log "fail." bad
     exit 1
   }
 }
-log "done"
+log "done" good
+log
 
+log
 log "install nerdtree.vim"
-which git >/dev/null || {
-  log "command git is not installed at all."
-  exit 1
-}
 test -d ~/.vim/bundle/nerdtree || {
   cd ~/.vim/bundle
   git clone https://github.com/scrooloose/nerdtree.git
   cd -
 }
-log "done"
+log "done" good
+log
+
+log
+log "install vim-json.vim"
+test -d ~/.vim/bundle/vim-json || {
+  cd ~/.vim/bundle
+  git clone https://github.com/rogerz/vim-json
+  cd -
+}
+log "done" good
+log
 
